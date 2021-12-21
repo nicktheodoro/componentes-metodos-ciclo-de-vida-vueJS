c48cca9807a7cff24e72539d3e858ac14ed1262a
nicktheodoro <nicolastheodoro@gmail.com>
2021-12-21 08:05:22 -0300

    Criando componentes

---
+++ criando-componentes/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<ButtonVue></ButtonVue>
+		<ButtonString></ButtonString>
+		<ButtonTemplateString></ButtonTemplateString>
+		<ButtonXTemplate></ButtonXTemplate>
+		<ButtonInline inline-template>
+			<button @click="click">
+				Click ButtonInline {{ count }} times
+			</button>
+		</ButtonInline>
+		<ButtonRender></ButtonRender>
+		<ButtonJSX></ButtonJSX>
+	</div>
+</template>
+
+<script>
+	import ButtonVue from "./ButtonVue.vue";
+
+	export default {
+		name: "App",
+		components: {
+			ButtonVue,
+		},
+	};
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+</style>
---
+++ criando-componentes/src/components.js
@@ -0,0 +1,109 @@
+import Vue from "vue";
+
+Vue.component("ButtonString", {
+  template:
+    '<button @click="click">Click ButtonString {{ count }} times</button>',
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    click() {
+      this.count++;
+    },
+  },
+});
+
+Vue.component("ButtonTemplateString", {
+  template: `
+    <button @click="click">
+      Click ButtonTemplateString {{ count }} times
+    </button>
+    `,
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    click() {
+      this.count++;
+    },
+  },
+});
+
+Vue.component("ButtonXTemplate", {
+  template: "#button-x-template",
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    click() {
+      this.count++;
+    },
+  },
+});
+
+Vue.component("ButtonInline", {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    click() {
+      this.count++;
+    },
+  },
+});
+
+Vue.component("ButtonRender", {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  render(createElement) {
+    return createElement(
+      "button",
+      {
+        on: {
+          click: () => this.count++,
+        },
+      },
+      [`Click ButtonRender ${this.count} times`]
+    );
+  },
+});
+
+Vue.component("ButtonJSX", {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    click() {
+      this.count++;
+    },
+  },
+  render() {
+    return (
+      <button onClick={this.click}>
+        Click ButtonJSX {this.count} times
+      </button>
+    );
+  },
+});
